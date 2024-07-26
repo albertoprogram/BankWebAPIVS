722edc6f72d5b90957013c70fe78c61685b5faae
Alberto Chirinos <developernet@outlook.com>
2024-07-25 22:17:52 -0500

    Customer entity

---
+++ BankWebAPIVS/Entities/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankWebAPIVS.Entities
+{
+    public class Customer
+    {
+        [Required(ErrorMessage = "The field {0} is required.")]
+        [StringLength(maximumLength: 10, ErrorMessage = "The field {0} must not be more than {1} characters.")]
+        public string CustomerId { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required.")]
+        [StringLength(maximumLength: 100, ErrorMessage = "The field {0} must not be more than {1} characters.")]
+        public string Name { get; set; }
+    }
+}
